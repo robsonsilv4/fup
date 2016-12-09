9aa9a732f098622dd304ae0a94651ed01d48edd2
robsonufc <robsonsilv4@alu.ufc.br>
2016-12-09 08:44:30 -0300

    Criada função para desenhar os jogadores

--- codigo.py
+++ codigo.py
@@ -10,6 +10,15 @@ fps = pygame.time.Clock()
 jogador1 = {'posicao': {'x': 40, 'y': 300}, 'raio': 15}
 jogador2 = {'posicao': {'x': 760, 'y': 300}, 'raio': 15}
 
+
+def desenha_personagem(personagem, cor):
+    pygame.draw.circle(
+        tela,
+        cor,
+        (personagem['posicao']['x'],
+         personagem['poscao']['y']),
+        personagem['raio'])
+
 while True:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
