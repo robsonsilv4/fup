915512f2b4a695c6700208b8ebb7bfcf189abe5c
robsonufc <robsonsilv4@alu.ufc.br>
2016-12-09 08:32:09 -0300

    Adicionado alguns comentários

--- codigo.py
+++ codigo.py
@@ -1,6 +1,6 @@
 import pygame
-import random
 
+# Inicialização do Pygame
 pygame.init()
 
 tela = pygame.display.set_mode((800, 600))
@@ -9,4 +9,5 @@ fps = pygame.time.Clock()
 while True:
 	for evento in pygame.event.get():
 		if evento.type == pygame.QUIT:
+			# Função do Pygame para fechar a janela
 			pygame.quit()
