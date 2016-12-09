3496e6ad37b2d65e8a2243150c3d41f5887a5a36
robsonufc <robsonsilv4@alu.ufc.br>
2016-12-09 08:52:52 -0300

    Inicio do jogo e melhorias

--- codigo.py
+++ codigo.py
@@ -17,20 +17,26 @@ azul_claro = (0, 255, 255)
 roxo = (255, 0, 255)
 
 # Jogadores
-jogador1 = {'posicao': {'x': 40, 'y': 300}, 'raio': 15}
-jogador2 = {'posicao': {'x': 760, 'y': 300}, 'raio': 15}
+jogador1 = {'pos': {'x': 40, 'y': 300}, 'raio': 15}
+jogador2 = {'pos': {'x': 760, 'y': 300}, 'raio': 15}
 
 
-def desenha_personagem(personagem, cor):
+def desenha_personagem(per, cor):
     pygame.draw.circle(
         tela,
         cor,
-        (personagem['posicao']['x'],
-         personagem['poscao']['y']),
-        personagem['raio'])
+        (per['pos']['x'],
+         per['pos']['y']),
+        per['raio'])
 
 while True:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             # Função do Pygame para fechar a janela
             pygame.quit()
+
+    tela.fill(branco)
+    desenha_personagem(jogador1, vermelho)
+    desenha_personagem(jogador2, vermelho)
+    pygame.display.update()
+    fps.tick(30)
