dbb9de2681c6297ec3c8111e469855b1d111ec5f
robsonufc <robsonsilv4@alu.ufc.br>
2016-12-09 15:59:44 -0300

    Várias coisas implementadas

--- codigo.py
+++ codigo.py
@@ -1,5 +1,11 @@
 import pygame
 import pygame.gfxdraw
+import math
+
+
+# Adicionar ao github:
+# Tratamento de colisão
+# Função de desenho generalizada
 
 # Inicialização do Pygame
 pygame.init()
@@ -20,24 +26,65 @@ roxo = (255, 0, 255)
 # Jogadores
 jogador1 = {'pos': {'x': 40, 'y': 300}, 'raio': 15}
 jogador2 = {'pos': {'x': 760, 'y': 300}, 'raio': 15}
+# n_balas = 3
+bala = {'pos': {'x': 40, 'y': 300}, 'vel': 15}
 
 
-def desenha_personagem(per, cor):
+def desenha_jogadores(per, raio, cor):
     pygame.gfxdraw.filled_circle(
         tela,
         per['pos']['x'],
         per['pos']['y'],
-        per['raio'],
+        raio,
         cor)
 
-    if per['pos']['y'] - per['raio'] < 0:
-    	print("Saiu y")
-    if per['pos']['y'] - per['raio'] > 600:
-    	print('Saiu y')
+    # Limites da tela
     if per['pos']['x'] - per['raio'] < 0:
-    	print('Saiu x')
+        print('Saiu x')
     if per['pos']['y'] - per['raio'] > 800:
-    	print('Saiu x')
+        print('Saiu x')
+    if per['pos']['y'] - per['raio'] < 0:
+        print("Saiu y")
+    if per['pos']['y'] - per['raio'] > 600:
+        print('Saiu y')
+
+
+def desenha_bala(bala, raio, cor, vel=30):
+    pygame.gfxdraw.filled_circle(
+        tela,
+        bala['pos']['x'],
+        bala['pos']['y'],
+        raio,
+        cor)
+
+    bala['vel']['y'] += 1
+    bala['pos'][x] += bala['vel']['x']
+    bala['pos']['y'] += bala['vel']['y']
+
+    if bala['pos']['y'] - raio < 0:
+        bala['pos']['y'] = 0 + raio
+        #bala['vel']['y'] *= -1
+    if bala['pos']['y'] + raio > 600:
+        bala['pos']['y'] = 600 + raio
+        #bala['vel']['y'] *= -1
+    if bala['pos']['x'] - raio < 0:
+        bala['pos']['x'] = 0 + raio
+        #bala['vel']['x'] *= -1
+    if bala['pos']['y'] - raio > 0:
+        bala['pos']['y'] = 800 - raio
+        #bala['vel']['y'] *= -1
+
+
+# Tratamento de colisão
+# Com base nas funções do Rubens
+
+
+def colisao(jog1, jog2):
+    colh = jog2['pos']['x'] - jog1['pos']['x']
+    colv = jog2['pos']['y'] - jog1['pos']['y']
+    res = math.sqrt(colh * colh + colv * colv)
+    if res <= jog1['raio'] + jog2['raio']:
+        print('Colidiu')
 
 while True:
     for evento in pygame.event.get():
@@ -68,7 +115,9 @@ while True:
                 jogador2["pos"]['y'] += 10
 
     tela.fill(branco)
-    desenha_personagem(jogador1, azul)
-    desenha_personagem(jogador2, vermelho)
+    desenha_jogadores(jogador1, 15, azul)
+    desenha_jogadores(jogador2, 15, vermelho)
+    desenha_bala(bala, 5, amarelo)
+    colisao(jogador1, jogador2)
     pygame.display.update()
     fps.tick(30)
