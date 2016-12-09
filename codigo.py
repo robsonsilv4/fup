dd2f9855804d8782652237c88cc933e6130cb2b7
robsonufc <robsonsilv4@alu.ufc.br>
2016-12-09 08:37:08 -0300

    Criado jogadores

--- codigo.py
+++ codigo.py
@@ -6,8 +6,12 @@ pygame.init()
 tela = pygame.display.set_mode((800, 600))
 fps = pygame.time.Clock()
 
+# Jogadores
+jogador1 = {'posicao': {'x': 40, 'y': 300}, 'raio': 15}
+jogador2 = {'posicao': {'x': 760, 'y': 300}, 'raio': 15}
+
 while True:
-	for evento in pygame.event.get():
-		if evento.type == pygame.QUIT:
-			# Função do Pygame para fechar a janela
-			pygame.quit()
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            # Função do Pygame para fechar a janela
+            pygame.quit()
