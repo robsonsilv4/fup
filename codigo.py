89aff888607acbfb98ca83238694c03ce2f747a8
robsonufc <robsonsilv4@alu.ufc.br>
2016-12-11 08:54:56 -0300

    Adicionando as balas e melhorando o código

--- codigo.py
+++ codigo.py
@@ -20,9 +20,12 @@ roxo = (255, 0, 255)
 # Jogadores
 jogador1 = {'pos': {'x': 40, 'y': 300}, 'raio': 15}
 jogador2 = {'pos': {'x': 760, 'y': 300}, 'raio': 15}
-# n_balas = 3
-bala = {'pos': {'x': 40, 'y': 300}, 'vel': 15}
+# bala = {'pos': {'x': 40, 'y': 300}, 'vel': 15}
 
+balas = []
+ang = 45
+vel = 30
+max_balas = 5
 
 def desenha_jogadores(per, raio, cor):
     pygame.gfxdraw.filled_circle(
@@ -51,37 +54,45 @@ def desenha_bala(bala, raio, cor):
         raio,
         cor)
 
-    #bala['vel']['y'] += 1
-    #bala['pos']['x'] += bala['vel']['x']
-    #bala['pos']['y'] += bala['vel']['y']
+def ataliza_bala(bala):
+    bala['vel']['y'] += 1
+    bala['pos']['x'] += bala['vel']['x']
+    bala['pos']['y'] += bala['vel']['y']
 
-    # if bala['pos']['y'] - raio < 0:
-    #    bala['pos']['y'] = 0 + raio
-    #bala['vel']['y'] *= -1
-    # if bala['pos']['y'] + raio > 600:
-    #    bala['pos']['y'] = 600 + raio
-    #bala['vel']['y'] *= -1
-    # if bala['pos']['x'] - raio < 0:
-    #    bala['pos']['x'] = 0 + raio
-    #bala['vel']['x'] *= -1
-    # if bala['pos']['y'] - raio > 0:
-    #    bala['pos']['y'] = 800 - raio
-    #bala['vel']['y'] *= -1
+def limita_bala(bala):
+    if bala['pos']['y'] - raio < 0:
+        bala['pos']['y'] = 0 + raio
+        bala['vel']['y'] *= -1
+    if bala['pos']['y'] + raio > 600:
+        bala['pos']['y'] = 600 - raio
+        bala['vel']['y'] *= -1
+    if bala['pos']['x'] - raio < 0:
+        bala['pos']['x'] = 0 + raio
+        bala['vel']['x'] *= -1
+    if bala['pos']['y'] - raio > 0:
+        bala['pos']['y'] = 800 - raio
+        bala['vel']['y'] *= -1
+
+def colisao_bala(bala, jogador):
+    i
 
 
 def atirar(jogador):
     bala['pos']['x'] = jogador['pos']['x'] + jogador['raio']
 
 
+def colisao(obj1, obj2):
+    colh = obj2['pos']['x'] - obj1['pos']['x']
+    colv = obj2['pos']['y'] - obj1['pos']['y']
+    res = math.sqrt(colh * colh + colv * colv)
+    return res
+
 # Tratamento de colisão
 # Com base nas funções do Rubens
 
 
-def colisao(jog1, jog2):
-    colh = jog2['pos']['x'] - jog1['pos']['x']
-    colv = jog2['pos']['y'] - jog1['pos']['y']
-    res = math.sqrt(colh * colh + colv * colv)
-    if res <= jog1['raio'] + jog2['raio']:
+def colisao_jogs(jog1, jog2):
+    if colisao(jog1, jog2) <= jog1['raio'] + jog2['raio']:
         print('Colidiu')
 
 while True:
@@ -119,7 +130,7 @@ while True:
     tela.fill(branco)
     desenha_jogadores(jogador1, 15, azul)
     desenha_jogadores(jogador2, 15, vermelho)
-    desenha_bala(bala, 5, amarelo)
-    colisao(jogador1, jogador2)
+    # desenha_bala(bala, 5, amarelo)
+    colisao_jogs(jogador1, jogador2)
     pygame.display.update()
     fps.tick(30)
